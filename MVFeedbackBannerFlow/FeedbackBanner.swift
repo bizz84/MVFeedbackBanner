7dd9f818637e0608e2c55084786acc2520caeb5c
Andrea Bizzotto <bizz84@gmail.com>
2016-09-28 23:23:43 +0100

    Add demo app

--- MVFeedbackBannerFlow/FeedbackBanner.swift
+++ MVFeedbackBannerFlow/FeedbackBanner.swift
@@ -17,5 +17,21 @@ class FeedbackBanner: UIView {
         negativeButton.buttonStyle = .border
         positiveButton.buttonStyle = .fill
     }
-   
+ 
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        let bundle = Bundle(for: FeedbackBanner.self)
+        
+        if let view = bundle.loadNibNamed("FeedbackBanner", owner: self, options: nil)?.first as? UIView {
+            
+            self.addSubview(view)
+            
+            view.topAnchor = self.topAnchor
+            view.leftAnchor = self.leftAnchor
+            view.rightAnchor = self.rightAnchor
+            view.bottomAnchor = self.bottomAnchor
+        }
+    }
+    
 }
---
+++ MVFeedbackBannerFlowDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVFeedbackBannerFlowDemo
+//
+//  Created by Andrea Bizzotto on 28/09/2016.
+//  Copyright © 2016 musevisions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
